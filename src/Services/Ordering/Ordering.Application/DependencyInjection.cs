11610dc962bfa31a625b487db2baf4cf04bcae49
hà văn dũng <83910596+havandung744@users.noreply.github.com>
2026-02-21 15:40:09 +0700

    Add ordering service and setup clean architecture for this  service

---
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+
+            // Add application services here, for example:
+
+            return services;
+        }
+    }
+}
