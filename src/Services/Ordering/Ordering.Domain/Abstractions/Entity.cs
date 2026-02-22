4605de729c5b245f9f7db7867545ed42128a026b
hà văn dũng <83910596+havandung744@users.noreply.github.com>
2026-02-22 12:22:21 +0700

    Develop DDD Abstractions (Entity, Aggregate, DomainEvent) in Ordering.Domain

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Entity.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Domain.Abstractions
+{
+    public abstract class Entity<T> : IEntity<T>
+    {
+        public abstract T Id { get; set; }
+        public abstract DateTime? CreateAt { get; set; }
+        public abstract string? CreatedBy { get; set; }
+        public abstract DateTime? LastModified { get; set; }
+        public abstract string? LastModifiedBy { get; set; }
+    }
+}
