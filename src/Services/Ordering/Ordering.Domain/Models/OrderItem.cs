8f7678a73587a284823d7fcff69c6d957c441f4d
hà văn dũng <83910596+havandung744@users.noreply.github.com>
2026-02-23 15:51:17 +0700

    Develop Strongly typed IDs for Order Entities

--- src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -1,8 +1,8 @@
 ﻿namespace Ordering.Domain.Models
 {
-    public class OrderItem : Entity<Guid>
+    public class OrderItem : Entity<OrderItemId>
     {
-        internal OrderItem(Guid orderId, Guid productId, int quantity, decimal price)
+        internal OrderItem(OrderId orderId, ProductId productId, int quantity, decimal price)
         {
             //Id = OrderItemId.Of(Guid.NewGuid());
             OrderId = orderId;
@@ -11,8 +11,8 @@
             Price = price;
         }
 
-        public Guid OrderId { get; private set; } = default!;
-        public Guid ProductId { get; private set; } = default!;
+        public OrderId OrderId { get; private set; } = default!;
+        public ProductId ProductId { get; private set; } = default!;
         public int Quantity { get; private set; } = default!;
         public decimal Price { get; private set; } = default!;
     }
