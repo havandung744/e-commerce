7a9893106a069cb6a536d79a88f7e4f1f0255d22
hà văn dũng <83910596+havandung744@users.noreply.github.com>
2026-02-24 15:16:54 +0700

    Develop DDD Rich-domain model Entity for Order Entities in Order.Domain Layer

--- src/Services/Ordering/Ordering.Domain/Models/Customer.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -4,5 +4,20 @@
     {
         public string Name { get; private set; } = default!;
         public string Email { get; private set; } = default!;
+
+        public static Customer Create(CustomerId id, string name, string email)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(name);
+            ArgumentException.ThrowIfNullOrEmpty(email);
+
+            var customer = new Customer
+            {
+                Id = id,
+                Name = name,
+                Email = email
+            };
+
+            return customer;
+        }
     }
 }
--- src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
@@ -3,5 +3,22 @@
     public record CustomerId
     {
         public Guid Value { get; }
+
+        private CustomerId(Guid value)
+        {
+            Value = value;
+        }
+
+        public static CustomerId Of(Guid value)
+        {
+            /// value type (struct) can not null?
+            //ArgumentNullException.ThrowIfNull(value);
+
+            if (value == Guid.Empty)
+            {
+                throw new DomainException("CustomerId cannot be empty.");
+            }
+            return new CustomerId(value);
+        }
     }
 }
