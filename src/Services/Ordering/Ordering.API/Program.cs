ae3cee5384044b26705443b1c9c8fe2f5434d16d
hà văn dũng <83910596+havandung744@users.noreply.github.com>
2026-07-12 23:04:02 +0700

    Create EF Core Migrations for ApplicationDbContext in Ordering.Infrastructure

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services
     .AddApplicationServices(builder.Configuration)
-    .AddInfrastructureServices()
+    .AddInfrastructureServices(builder.Configuration)
     .AddAPIServices();
 var app = builder.Build();
 
--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Infrastructure.Data;
 
 namespace Ordering.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
+            var connectionString = configuration.GetConnectionString("DatabaseConnection");
 
-            // Add application services here, for example:
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
 
             return services;
         }
