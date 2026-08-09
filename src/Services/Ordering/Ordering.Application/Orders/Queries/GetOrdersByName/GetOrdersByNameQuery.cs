8c89d9000f72a87db334c518d1da4a6d3fde069f
hà văn dũng <83910596+havandung744@users.noreply.github.com>
2026-08-09 12:57:05 +0700

    add query GetOrdersByName

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameQuery.cs
@@ -0,0 +1,8 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrdersByName
+{
+    public record GetOrdersByNameQuery(string name) : IQuery<GetOrdersByNameResult>
+    {
+    }
+
+    public record GetOrdersByNameResult(IEnumerable<OrderDto> Orders);
+}
